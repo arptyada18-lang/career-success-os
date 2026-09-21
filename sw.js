7007be102283a56f48d700bce5276211be60213a
arptyada18-lang <arptyada18@gmail.com>
2026-09-21 21:28:13 +0530

    Cache Advanced V7 runtime for offline use

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="career-success-os-v6";
-const ASSETS=["./","./index.html","./style.css","./app.js","./v3.js","./v4.js","./v5.js","./v6.js","./manifest.json","./icon.svg"];
+const CACHE="career-success-os-v7";
+const ASSETS=["./","./index.html","./style.css","./app.js","./v3.js","./v4.js","./v5.js","./v6.js","./v7.js","./manifest.json","./icon.svg"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",event=>{
